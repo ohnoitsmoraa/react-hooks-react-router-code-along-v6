278404b5946d6fe3d85297ff147ac0c7718f0dfd
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-08-13 08:37:37 +0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import "./NavBar.css"
+
+
+function NavBar() {
+  return (
+    <nav>
+        <NavLink to= "/" className="nav-link">Home</NavLink>
+        <NavLink to= "/about" className="nav-link">About</NavLink>
+        <NavLink to= "/login" className="nav-link">Login</NavLink>
+    </nav>
+  )
+}
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import routes from "./routes.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./components/About";
+// import Login from "./components/Login";
+// import UserProfile from "./pages/UserProfile";
+// import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
